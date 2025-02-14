2ca98815cc49398a23ef1b3970964c187705a5cd
steve <emailstevekim@gmail.com>
2025-02-14 15:05:30 -0500

    Update metars.py

--- src/metars.py
+++ src/metars.py
@@ -5,15 +5,14 @@ import requests
 from dotenv import load_dotenv
 
 INSTRUCTIONS = (
-    "You are a Toronto historian/walking tour guide knowledgable in Toronto architecture, urban history, city planning sdata stored in JSON format. "
+    "You are a Toronto historian/walking tour guide knowledgable in Toronto architecture, urban history, city planning data stored in JSON format. "
     "You are expert at reading street and building data for a given location and providing an English description of the urban geography at that location. "
-    "The English description should be in the style of a walking tour."
+    "The English description should be in the style of a walking tour and no more than 280 characters."
 )
 PROMPT = (
     "Briefly describe the history, culture and architecture for the location identified in the following JSON data. "
     "Format your response as a JSON object with the following key: 'street_description'. "
-    "OPENSTREETMAP DATA"
-    "{ 'type': 'Feature', 'properties': { 'name': 'Old City Hall' }, 'geometry': { 'type': 'Point', 'coordinates': [ -79.381759498506327, 43.652636575812757 ] } }"
+    # "{ 'type': 'Feature', 'properties': { 'name': 'Old City Hall' }, 'geometry': { 'type': 'Point', 'coordinates': [ -79.381759498506327, 43.652636575812757 ] } }"
 )
 
 def gpt_list_models(client):
@@ -34,28 +33,38 @@ def gpt_chat(client: openai.OpenAI, model: str, messages: list, max_tokens: int)
     return weather_report
 
 
-def gpt_weather_report(client: openai.OpenAI, model: str, max_tokens: int):
+def gpt_osm_description(place: dict, client: openai.OpenAI, model: str, max_tokens: int):
     msgs = [{"role": "system", "content": INSTRUCTIONS}]
     content = [
         {
             "type": "text",
-            "text": f"{PROMPT}",
+            "text": f"{PROMPT} \n\nOPENSTREETMAP DATA: {json.dumps(place)}",
         }
     ]
     msgs.append({"role": "user", "content": content})
     response = gpt_chat(
         client=client, model=model, messages=msgs, max_tokens=max_tokens
     )
-    print(response)
+    return response
 
 def main():
+
+    with open("data/osm.geojson", "r") as places_file:
+        places = json.load(places_file)
+        # print(places)
+    
     load_dotenv() # OpenAI API Key is stored in an environment variable
     openai_client = openai.OpenAI()
-    gpt_weather_report(
-        client=openai_client, model="gpt-4o-mini", max_tokens=1000
-    )
-    # with open("data/metars/current-weather.json", "w") as f:
-        # json.dump(metars, f, indent=4)
+    for feat in places["features"]:
+        place = feat["properties"]
+        description = gpt_osm_description(
+            client=openai_client, model="gpt-4o-mini", max_tokens=1000, place=place
+        )
+        feat["description"] = description
+        # print(place, description)
+    
+    with open("data/osm-descriptions.json", "w") as f:
+        json.dump(places, f, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
